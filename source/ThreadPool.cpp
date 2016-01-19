96804989b168a763dc095aee75a9e19efdbf4e5a
Jeffrey Johnson <jeffcjohnson@live.com>
2016-01-18 23:32:28 -0800

    First try at thread pool

---
+++ source/ThreadPool.cpp
@@ -0,0 +1,85 @@
+#include "ThreadPool.h"
+
+void ThreadPool::Job::queue()
+{
+    pool->jobLock.lock();
+    pool->readyQueue.insert(this);
+    pool->jobLock.unlock();
+}
+
+void ThreadPool::Job::addDependency(Job* other)
+{
+    dependencies.insert(other);
+    other->dependents.insert(other);
+}
+
+bool ThreadPool::jobComparator(Job* first, Job* second)
+{
+    return first->priority > second->priority;
+}
+
+ThreadPool::Job::Job(std::function<void()> func, ThreadPool* pool) : pool(pool), func(func)
+{
+}
+
+ThreadPool::ThreadPool(size_t threadCount) : threadCount(threadCount > 0 ? threadCount : std::thread::hardware_concurrency()), readyQueue(jobComparator)
+{
+    for (size_t i = 0; i < threadCount; i++)
+    {
+        threads.emplace_back(&runThread, this, i);
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+    shutdown = true;
+    for(auto& thread : threads)
+    {
+        thread.join();
+    }
+}
+
+ThreadPool::Job* ThreadPool::createJob(const std::function<void()>& func)
+{
+    return new Job(func, this);
+}
+
+bool ThreadPool::completed(Job* job)
+{
+    bool ret;
+    jobLock.lock();
+    ret = readyQueue.count(job) == 0;
+    jobLock.unlock();
+    return ret;
+}
+
+void ThreadPool::wait(Job* job)
+{
+    while(!completed(job)) std::this_thread::yield();
+}
+
+void ThreadPool::runThread(int id)
+{
+    while(!shutdown)
+    {
+        jobLock.lock();
+        for(auto job : readyQueue)
+        {
+            if (job->dependencies.size() > 0) continue;
+            if (job->affinity >= 0 && job->affinity != id) continue;
+            readyQueue.erase(job);
+            jobLock.unlock();
+
+            job->func();
+
+            jobLock.lock();
+            for (auto dependent : job->dependents)
+            {
+                dependent->dependencies.erase(job);
+            }
+            delete job;
+            break;
+        }
+        jobLock.unlock();
+    }
+}
---
+++ source/include/ThreadPool.h
@@ -0,0 +1,52 @@
+#ifndef INCLUDE_THREADPOOL_H
+#define INCLUDE_THREADPOOL_H
+
+#include "ForwardDecs.h"
+#include <functional>
+#include <set>
+#include <list>
+#include <thread>
+#include <mutex>
+
+class ThreadPool
+{
+public:
+    class Job
+    {
+    public:
+        friend ThreadPool;
+
+        int affinity = -1;
+        int priority = 0;
+
+        void queue();
+        void addDependency(Job* other);
+        Job(std::function<void()> func, ThreadPool* pool);
+
+    private:
+        ThreadPool* const pool;
+        std::function<void()> func;
+        std::set<Job*> dependencies;
+        std::set<Job*> dependents;
+    };
+
+    explicit ThreadPool(size_t threadCount = 0);
+    ~ThreadPool();
+
+    const size_t threadCount;
+    bool shutdown = false;
+
+    Job* createJob(const std::function<void()>& func);
+    bool completed(Job* job);
+    void wait(Job* job);
+
+private:
+    static bool jobComparator(Job* first, Job* second);
+    std::set<Job*, bool(Job*, Job*)> readyQueue;
+    std::list<std::thread> threads;
+    std::mutex jobLock;
+
+    void runThread(int id);
+};
+
+#endif
